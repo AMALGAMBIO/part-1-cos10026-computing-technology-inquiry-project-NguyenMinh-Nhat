de9faf4df07e69b7c92d8c003ad7137515d77ba5
Nguyen Minh Nhat <wereareone0099@gmail.com>
2025-03-06 21:14:33 +0700

    week9

---
+++ Week9/cars.php
@@ -0,0 +1,28 @@
+<?php
+    require_once "settings.php";
+    $dbconn = @mysqli_connect($host,$user,$pwd, $sql_db);
+    if ($dbconn) {
+        $query =" SELECT `model`, `make`, car_id, price, yom FROM cars WHERE `price` <= 15000";
+        $result = mysqli_query($dbconn,$query);
+        if ($result) {
+            echo "<table border='1'>";
+        echo "<tr><th>ID</th><th>Make</th><th>Model</th><th>Price</th><th>Year</th></tr>";
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>" . $row['car_id'] . "</td>";
+            echo "<td>" . $row['make'] . "</td>";
+            echo "<td>" . $row['model'] . "</td>";
+            echo "<td>" . $row['price'] . "</td>";
+            echo "<td>" . $row['yom'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "<p>There's no cars to displayed: " . mysqli_error($dbconn) . "</p>";
+    }
+        mysqli_close($dbconn);  
+    } else {echo "<p>Unable to connect to the db.</p>";
+    }
+
+?>
